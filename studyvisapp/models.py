54f0fc4edacb80768450d8186ff11ac8c834490b
TMdiesel <cerezzodora2262@icloud.com>
2021-08-13 03:20:38 +0000

    fix: 時間がnoneで保存されたときの例外処理を追加

--- studyvisapp/models.py
+++ studyvisapp/models.py
@@ -9,13 +9,16 @@ CHOICE = (("danger", "機械学習"), ("warning", "統計"), ("primary", "読書
 class TimeModel(models.Model):
     item = models.CharField(max_length=50, choices=CHOICE)
     memo = models.TextField(null=True, blank=True)
-    starttime = models.DateTimeField()
-    endtime = models.DateTimeField()
+    starttime = models.DateTimeField(null=True, blank=True, default=None)
+    endtime = models.DateTimeField(null=True, blank=True, default=None)
     duration = models.DurationField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.get_item_display()}"
 
     def save(self, *args, **kwargs):
-        self.duration = self.endtime - self.starttime
+        try:
+            self.duration = self.endtime - self.starttime
+        except TypeError:  # 時間がNoneで保存された場合
+            pass
         super().save(*args, **kwargs)
--- studyvisapp/templatetags/poll_extras.py
+++ studyvisapp/templatetags/poll_extras.py
@@ -9,9 +9,11 @@ def rm_second(td: timedelta):
     def _add_zero(val):
         return "0" + str(val) if val < 10 else str(val)
 
-    total_seconds = int(td.total_seconds())
-    hours = total_seconds // 3600
-    minutes = (total_seconds % 3600) // 60
-    minutes = _add_zero(minutes)
-
-    return f"{hours}:{minutes}"
+    try:
+        total_seconds = int(td.total_seconds())
+        hours = total_seconds // 3600
+        minutes = (total_seconds % 3600) // 60
+        minutes = _add_zero(minutes)
+        return f"{hours}:{minutes}"
+    except AttributeError:
+        return td
